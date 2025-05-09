2b48d9f540a380f1cecf17c4ca18149b7c723e76
Luis Sánchez <luis.sanchezm86@gmail.com>
2025-05-09 17:01:01 -0400

    feat: Update service URLs and Keycloak configuration in Docker setup

--- User/Program.cs
+++ User/Program.cs
@@ -10,7 +10,7 @@ using Microsoft.OpenApi.Models;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://0.0.0.0:5150", "https://0.0.0.0:7150");
+builder.WebHost.UseUrls("http://*:80", "https://*:443");
 
 // Rest of the file remains unchanged...
 // [Previous content of Program.cs here]
